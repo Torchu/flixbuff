3c54badc2d4dc6ca6483ac5213b2e1da129f3881
torchu <victor.cabritag@gmail.com>
2022-05-22 15:24:40 +0200

    fix route

--- scripts/order_dict.py
+++ scripts/order_dict.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-DICT_PATH = '.github/workflows/allowed_words.txt'
+DICT_PATH = '.github/actions/check-spelling/allowed_words.txt'
 
 with open(DICT_PATH, mode='r') as f:
     file = f.read().split()
