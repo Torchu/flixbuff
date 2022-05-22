555259d490c511975765764cb78e712d4f83a689
torchu <victor.cabritag@gmail.com>
2022-05-22 15:22:34 +0200

    adds spellcheck to CI

---
+++ scripts/spellcheck.sh
@@ -0,0 +1,35 @@
+# Comprueba que los ficheros estén escritos correctamente
+
+#!/bin/bash
+SpellingError () {
+    error=0;
+    echo "";
+    echo "Analizando si hay fallos en fichero" $1":";
+    n_line=1;
+    while IFS= read -r line
+    do
+        resultado=$(echo $line | aspell --mode=tex  --lang=en --list | aspell --mode=tex  --lang=es --list --home-dir=. --personal=.github/actions/check-spelling/allowed_words.txt);
+        if [[ ! -z "$resultado" ]]
+        then
+         echo "Spell error in line $n_line : $resultado"
+         let "error = 1";
+        fi
+        let "n_line += 1"
+    done < $1
+    return $error;
+}
+export -f SpellingError;
+exitValue=0
+
+# Check ReadMe.md
+SpellingError README.md
+let "exitValue += $?"
+
+# Check docs
+for file in $(find ./doc -name "*.tex")
+do
+    SpellingError $file
+    let "exitValue += $?"
+done
+
+exit $exitValue
