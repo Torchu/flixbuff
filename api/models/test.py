e21ae63ae6d0b4bb94f0440a7cc65ddb7c1e90b5
torchu <victor.cabritag@gmail.com>
2022-07-25 15:44:39 +0200

    fixes test class and tests lint

--- api/models/test.py
+++ api/models/test.py
@@ -1,5 +1,12 @@
-import os
+"""Dummy module for testing purposes."""
 
 class Test:
-    def greet(self):
+    """Testing class."""
+
+    def greet(self) -> str:
+        """Returns a greeting."""
         return "Hello World!"
+
+    def personal_greeting(self, name: str) -> str:
+        """Returns a personal greeting."""
+        return f"Hello {name}!"
