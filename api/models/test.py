b1e023531691867f8730dfcd3deadd632de5c2f4
Víctor Cabrita Gómez <38671513+Torchu@users.noreply.github.com>
2022-07-27 17:18:47 +0200

    Set up ci, updates #5 #6 (#16)
    * removes template files
    
    * adds api folder
    
    * removes package.json
    
    * set up pylint
    
    * fix github action
    
    * adds dummy class
    
    * updates gitignore
    
    * adds test
    
    * adds paths to actions
    
    * adds full ci on pull request
    
    * adds full ci on pull request
    
    * fixes test class and tests lint
    
    * removes ci duplicates
    
    * updates checkout version
    
    * adds jobs dependency
    
    * adds poetry
    
    * fix working directory
    
    * recovers lost python version

---
+++ api/models/test.py
@@ -0,0 +1,12 @@
+"""Dummy module for testing purposes."""
+
+class Test:
+    """Testing class."""
+
+    def greet(self) -> str:
+        """Returns a greeting."""
+        return "Hello World!"
+
+    def personal_greeting(self, name: str) -> str:
+        """Returns a personal greeting."""
+        return f"Hello {name}!"
