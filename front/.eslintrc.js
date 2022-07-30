6dbb2d0180b2cc1f284d72bbc01efc0fb014ec29
torchu <victor.cabritag@gmail.com>
2022-07-30 20:51:41 +0200

    set up lint

---
+++ front/.eslintrc.js
@@ -0,0 +1,117 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@angular-eslint/recommended',
+    'plugin:@angular-eslint/template/process-inline-templates'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json',
+    extraFileExtensions: '.html'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {},
+  overrides: [
+    {
+      files: '*.ts',
+      rules: {
+        curly: ['error', 'all'],
+        'lines-between-class-members': 'off',
+        'max-len': ['error', { code: 120, ignorePattern: '^import .*' }],
+        'no-restricted-imports': ['error', { paths: ['rxjs/Rx'] }],
+        quotes: ['error', 'single', {allowTemplateLiterals: true}],
+        'sort-imports': 'error',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-parameter-properties': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^ignored?$' }],
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'default',
+            format: ['camelCase']
+          },
+          {
+            selector: 'memberLike',
+            modifiers: ['private'],
+            format: ['camelCase'],
+            leadingUnderscore: 'allow'
+          },
+          {
+            selector: 'memberLike',
+            format: ['camelCase'],
+            leadingUnderscore: 'allow'
+          },
+          {
+            selector: 'parameter',
+            format: ['camelCase'],
+            leadingUnderscore: 'allow'
+          },
+          {
+            selector: 'variable',
+            format: ['camelCase', 'UPPER_CASE'],
+          },
+          {
+            selector: 'typeLike',
+            format: ['PascalCase']
+          },
+          {
+            selector: 'enumMember',
+            format: ['PascalCase']
+          }
+        ],
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase'
+          }
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case'
+          }
+        ],
+        indent: ['error', 2, { SwitchCase: 1 }]
+      }
+    },
+    {
+      files: '*.js',
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    },
+    {
+      files: '*.spec.ts',
+      rules: {
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'sort-imports': 'off'
+      }
+    },
+    {
+      files: '*.html',
+      extends: 'plugin:@angular-eslint/template/recommended',
+      rules: {
+        'max-len': 'off',
+        indent: 'off'
+      }
+    }
+  ]
+}
