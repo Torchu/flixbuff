c1f99ef1a7df0e85673bb026fc6816d154e2718a
torchu <victor.cabritag@gmail.com>
2022-08-18 17:34:46 +0200

    adds bib

--- api/config/shows_api_config.py
+++ api/config/shows_api_config.py
@@ -6,6 +6,3 @@ load_dotenv()
 SHOWS_DB_API = 'https://api.themoviedb.org/3/tv'
 SHOWS_DB_API_KEY = os.environ.get('SHOWS_DB_API_KEY')
 SHOWS_DB_LANGUAGE = os.environ.get('SHOWS_DB_LANGUAGE', 'es_ES')
-
-DEBUG_MODE = os.environ.get('DEBUG_MODE', True)
-API_PORT = os.environ.get('API_PORT', 5000)
