14e25f486be947a7fe8fda3ebc8cc19ebd8cecf5
torchu <victor.cabritag@gmail.com>
2022-08-29 13:17:54 +0200

    fixes import

--- api/tests/review_test.py
+++ api/tests/review_test.py
@@ -1,6 +1,6 @@
 """Test module for the Review model."""
 import pytest
-from models.review import Review, ShowInfo
+from models.review import Review, SeasonInfo
 
 
 class TestReview():
@@ -8,10 +8,10 @@ class TestReview():
 
     def test_initialize(self):
         """Tests the initialization of the Review model."""
-        review = Review('Juan Antonio', ShowInfo('Cosas rarunas', 1, 'Season 1'), "This is a review", 10)
+        review = Review('Juan Antonio', SeasonInfo('Cosas rarunas', 1, 'Season 1'), "This is a review", 10)
         assert isinstance(review, Review)
 
     def test_rating_exception(self):
         """Test that and exception is raised when the rating is not between 0 and 10."""
         with pytest.raises(ValueError):
-            Review('Juan Antonio', ShowInfo('Cosas rarunas', 1, 'Season 1'), "This is a review", 11)
+            Review('Juan Antonio', SeasonInfo('Cosas rarunas', 1, 'Season 1'), "This is a review", 11)
