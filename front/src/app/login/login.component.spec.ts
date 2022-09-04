51e2c5afff88fb205c4b26ceae7c58cc5cea808f
torchu <victor.cabritag@gmail.com>
2022-09-04 21:20:28 +0200

    fixes tests

--- front/src/app/login/login.component.spec.ts
+++ front/src/app/login/login.component.spec.ts
@@ -1,4 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +16,19 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MatSnackBarModule,
+        MatTabsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule
+      ],
+      providers: [{ provide: MatDialogRef, useValue: {} }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
