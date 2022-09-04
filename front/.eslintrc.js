70e7f798e3ec84c688dedae9667b2cbba4827709
Víctor Cabrita Gómez <38671513+Torchu@users.noreply.github.com>
2022-09-04 13:30:05 +0200

    Adds navigation bar (#73)
    * Adds navigation bar
    
    * fixes cannot read file eslint
    
    * removes unused test

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: 'tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
     extraFileExtensions: '.html'
   },
   plugins: ['@typescript-eslint'],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BaseComponent } from './base/base.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, BaseComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
