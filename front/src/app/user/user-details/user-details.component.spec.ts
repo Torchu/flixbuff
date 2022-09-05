acb01dbb04e6b77065a465ef36dfd1efbce2d113
torchu <victor.cabritag@gmail.com>
2022-09-05 21:38:27 +0200

    fixes tests

--- front/src/app/user/user-details/user-details.component.spec.ts
+++ front/src/app/user/user-details/user-details.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserDetailsComponent } from './user-details.component';
 
@@ -8,7 +13,8 @@ describe('UserDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserDetailsComponent]
+      declarations: [UserDetailsComponent],
+      imports: [HttpClientModule, RouterTestingModule, MatSnackBarModule, MatIconModule, MatCardModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserDetailsComponent);
