01a7106c058e982812d72f73cd43157bef387a4a
torchu <victor.cabritag@gmail.com>
2022-09-05 10:07:57 +0200

    remove unused import

--- front/src/app/base/base.component.spec.ts
+++ front/src/app/base/base.component.spec.ts
@@ -3,7 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { BaseComponent } from './base.component';
