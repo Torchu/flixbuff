f42c27b577c186d8dd94f2eeb29aa77f0c64e425
torchu <victor.cabritag@gmail.com>
2022-09-05 20:06:26 +0200

    fixes tests

--- front/src/app/user/user-list/user-list.component.spec.ts
+++ front/src/app/user/user-list/user-list.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UserListComponent } from './user-list.component';
 
@@ -8,7 +14,16 @@ describe('UserListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserListComponent]
+      declarations: [UserListComponent],
+      imports: [
+        HttpClientModule,
+        MatSnackBarModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserListComponent);
