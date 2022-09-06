91a38fcf612753fa6acfd3a8d13dfc045ed058fd
Víctor Cabrita Gómez <38671513+Torchu@users.noreply.github.com>
2022-09-06 20:25:26 +0200

    FB-28 sets up front server (#106)
    * adds build configuration
    
    * adds type to prod environment
    
    * add engines to package.json
    
    * adds front server file
    
    * fixes lint

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
-export const environment = {
-  production: true
+import { EnvironmentInterface } from 'src/interfaces/environment.interface';
+
+export const environment: EnvironmentInterface = {
+  production: true,
+  frontendDomain: 'https://flixbuff-front.herokuapp.com',
+  apiDomain: 'flixbuff-api.herokuapp.com',
+  apiProtocol: 'https'
 };
